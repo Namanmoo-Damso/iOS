fc035c9ab0eb2c83f4de64abe9211da0180f1b7e
dvktdr78 <kei1221@ajou.ac.kr>
2025-12-26 01:03:24 +0900

    Initial Commit

---
+++ damso/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  damso
+//
+//  Created by ilim on 2025-12-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ damso/damsoApp.swift
@@ -0,0 +1,17 @@
+//
+//  damsoApp.swift
+//  damso
+//
+//  Created by ilim on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct damsoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
